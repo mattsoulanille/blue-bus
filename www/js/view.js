6ed7b837dbef13acb489977aca7b5e8ba1cd313f
pThiel157 <thiel157@gmail.com>
2018-02-03 13:48:12 -0500

    Adjusted css

--- www/js/view.js
+++ www/js/view.js
@@ -6,11 +6,15 @@ function setView(){
     $('#currentViewDesc').text('Going to Bryn Mawr');
     $('#currentViewHeader').removeClass('bmc-header');
     $('#currentViewHeader').addClass('hc-header');
+    $('#currentViewDesc').removeClass('bmc-desc');
+    $('#currentViewDesc').addClass('hc-desc');
   }
   else {
     $('#currentViewHeader').text('Bryn Mawr');
     $('#currentViewDesc').text('Going to Haverford');
     $('#currentViewHeader').removeClass('hc-header');
-    $('#currentViewHeader').addClass('bmc-header');  
+    $('#currentViewHeader').addClass('bmc-header');
+    $('#currentViewDesc').removeClass('bmc-desc');
+    $('#currentViewDesc').addClass('hc-desc'); 
   }
 }
