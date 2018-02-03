bad886b3fbaebe15c7ba996d2bc1fe4c15b35da0
pThiel157 <thiel157@gmail.com>
2018-02-03 17:14:37 -0500

    Cleaned up UI

--- www/js/nav.js
+++ www/js/nav.js
@@ -4,8 +4,8 @@ $(document).ready(function(event){
   $('#HaverfordButton').click(function(){
     MAINVIEW = "HAVERFORD"
     setView();
-    $(this).css({'transform': 'scale(0.85)'})
-    $('#BrynMawrButton').css({'transform': 'scale(1)'})
+    $(this).css({'transform':'scale(0.85)', 'opacity':'0.6'})
+    $('#BrynMawrButton').css({'transform':'', 'opacity':''})
     setView();
   });
 
@@ -13,7 +13,7 @@ $(document).ready(function(event){
   $('#BrynMawrButton').click(function(){
     MAINVIEW = "BRYNMAWR";
     setView();
-    $(this).css({'transform': 'scale(0.85)'})
-    $('#HaverfordButton').css({'transform': 'scale(1)'})
+    $(this).css({'transform':'scale(0.85)', 'opacity':'0.6'})
+    $('#HaverfordButton').css({'transform':'', 'opacity':''})
   });
 });
