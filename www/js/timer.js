406de6301684992505e16ce465216b379aeb3658
Silvia <ssusai@haverford.edu>
2018-02-02 23:34:35 -0500

    changed clock to show leading 0s

---
+++ www/js/timer.js
@@ -0,0 +1,46 @@
+// From https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  return {
+    'total': t,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var timeinterval = setInterval(function(){
+    var t = getTimeRemaining(endtime);
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+  	minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+  	secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+   
+    if(t.total<=0){
+      clearInterval(timeinterval);
+    }
+  },1000);
+}
+
+function updateClock(id){
+  var clock = document.getElementById(id);
+  var t = getTimeRemaining(endtime);
+  hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+  minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+  secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  clock.innerHTML = 'hours: '+ t.hours + '<br>' +
+                    'minutes: ' + t.minutes + '<br>' +
+                    'seconds: ' + t.seconds;
+  if(t.total<=0){
+  	//get the new date now
+    clearInterval(timeinterval);
+  }
+}
+
+updateClock(); // run function once at first to avoid delay
+var timeinterval = setInterval(updateClock,1000);
