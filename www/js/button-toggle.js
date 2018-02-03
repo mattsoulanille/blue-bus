eeb513a3d9f71bf9f54c3869b0e8673b038c1d3e
pThiel157 <thiel157@gmail.com>
2018-02-03 12:36:10 -0500

    Tied button presses to haverford/bryn mawr view changes

--- www/js/button-toggle.js
+++ www/js/button-toggle.js
@@ -1,30 +1,30 @@
 //General info on queryselector found here: https://www.w3schools.com/jsref/met_document_queryselector.asp
 //syntax for jquery found here: https://www.w3schools.com/jquery/
 //onclick using javascript found here: https://www.w3schools.com/jsref/event_onclick.asp
-
-function toHaverfordView(){
-	$("#haverford-clock").setAttribute('style', 'display:none;');
-	$("#brynmawr-clock").setAttribute('style', 'display:block;');
-}
-
-function toHaverfordView(){
-	$("#brynmawr-clock").setAttribute('style', 'display:none;');
-	$("#haverford-clock").setAttribute('style', 'display:block;');
-}
-
-function changeText(){
-	//chnanging text with jquery found here: http://api.jquery.com/html/
-	$("#haverford-clock").text("Hello, world");
-}
-
-function changeText2(){
-	document.getElementByID("haverford-clock").innerHTML = "Hello, world";
-}
-
-$(document).ready(function(){
-//	$("#HaverfordButton").click(function(){changeText()});
-	$("#HaverfordButton").click(function(){
-		changeText();
-	});
-
-});
+// 
+// function toHaverfordView(){
+// 	$("#haverford-clock").setAttribute('style', 'display:none;');
+// 	$("#brynmawr-clock").setAttribute('style', 'display:block;');
+// }
+//
+// function toHaverfordView(){
+// 	$("#brynmawr-clock").setAttribute('style', 'display:none;');
+// 	$("#haverford-clock").setAttribute('style', 'display:block;');
+// }
+//
+// function changeText(){
+// 	//chnanging text with jquery found here: http://api.jquery.com/html/
+// 	$("#haverford-clock").text("Hello, world");
+// }
+//
+// function changeText2(){
+// 	document.getElementByID("haverford-clock").innerHTML = "Hello, world";
+// }
+//
+// $(document).ready(function(){
+// //	$("#HaverfordButton").click(function(){changeText()});
+// 	$("#HaverfordButton").click(function(){
+// 		changeText();
+// 	});
+//
+// });
--- www/js/nav.js
+++ www/js/nav.js
@@ -1,9 +1,16 @@
-
-$(document).ready(function(){
+$(document).ready(function(event){
+  //alert("working");
   $('#HaverfordButton').click(function(){
-    
+    MAINVIEW = "HAVERFORD"
+    updateClock();
+    $('#main-clock').css({'background': 'red'});
   });
 
+  $('#BrynMawrButton').click(function(){
+    MAINVIEW = "BRYNMAWR";
+    updateClock();
+    $('#main-clock').css({'background': 'blue'});
+  });
 
   $('#HaverfordButton').on('tap', function(){
     alert("wow!");
